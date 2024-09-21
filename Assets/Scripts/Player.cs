bd51409d422ed5fba2c3124f75be1131b8c36990
sebastian <sebastianvillarroelchoque@gmail.com>
2024-09-20 21:26:27 -0400

    Versión inicial del proyecto Unity

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+
+[RequireComponent()]
+public class Player : MonoBehaviour
+{
+    public float maxJumpHeight = 4;
+    public float minJumpHeight = 1;
+    public float timeToJumpApex = .4f;
+    public float moveSpeed = 6;
+    public Vector2 wallJumpClimb;
+    public Vector2 wallJumpOff;
+    public Vector2 wallLeap;
+    public float wallSlideSpeedMax = 3;
+    public float wallStickTime = .25f;
+
+    private float accelerationTimeAirborne = .2f;
+    private float accelerationTimeGrounded = .1f;
+    private float gravity;
+    private float maxJumpVelocity;
+    private float minJumpVelocity;
+    private Vector3 velocity;
+    private float velocityXSmoothing;
+
+    private bool wallSliding;
+    private int wallDirX;
+    private float timeToWallUnstick;
+
+    void Start()
+    {
+  
+    }
+
+    public void SetDirectionalInput(Vector2 input)
+    {
+        directionalInput = input;
+    }
+
+    public void OnJumpInputDown()
+    {
+        if (wallSliding)
+        {
+            if (wallDirX == directionalInput.x)
+            {
+                velocity.x = -wallDirX * wallJumpClimb.x;
+                velocity.y = wallJumpClimb.y;
+            }
+            else if (directionalInput.x == 0)
+            {
+                velocity.x = -wallDirX * wallJumpOff.x;
+                velocity.y = wallJumpOff.y;
+            }
+            else
+            {
+                velocity.x = -wallDirX * wallLeap.x;
+                velocity.y = wallLeap.y;
+            }
+        }
+        else if (controller.collisions.below)
+        {
+            velocity.y = maxJumpVelocity;
+        }
+    }
+
+    public void OnJumpInputUp()
+    {
+        if (velocity.y > minJumpVelocity)
+        {
+            velocity.y = minJumpVelocity;
+        }
+    }
+
+
+
+
+
+
+ 
+}
